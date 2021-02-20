29ebf243f563800ee3cb732eb6b25bb7d338f446
davisteindorfer <36707311+davisteindorfer@users.noreply.github.com>
2021-02-20 17:44:37 -0300

    Delete Botão.js

--- Botão.js
+++
@@ -1,7 +0,0 @@
-var button = document.createElement("button");
-button.innerHTML = "Aperta Aqui!";
-
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-button.addEventListener ("click", function() {button.style.display = "none";} );
